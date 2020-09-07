c5a9c926fa5ec4e5a7b602a5f6160ffd357bc389
Mubeen Malik <mmmubeen761@gmail.com>
2020-09-07 16:02:28 +0500

    PracticebyMaryam

---
+++ app.js
@@ -0,0 +1,56 @@
+// Day 1 Exercise
+
+
+// challenge 1)
+// comments can make code readable 
+
+// challenge 2)
+// Welcome to 30DaysOfJavaScript
+
+// challenge 3)
+/*comments can make code readable, 
+easy to reuse and informative */
+
+// challenge 4)
+var string = "This is String";
+var boolean = true;
+var undefi = undefined;
+var nul = null;
+
+// challenge 5)
+console.log(typeof 8)
+console.log(typeof "malik")
+console.log(typeof true)
+console.log(typeof null)
+console.log(typeof undefined)
+
+// challenge 6)
+var var1;
+var var2;
+var var3;
+var var4;
+
+// challenge 7)
+var var1 = "malik";
+var var2 = 8+9;
+var var3 = "khan";
+var var4 = 2*2;
+
+// challenge 8)
+var firstName = "malik";
+var lastName = "mubeen";
+var country = "Pakistan";
+var age = "18";
+
+// challenge 9)
+var firstName = "malik",
+    lastName = "mubeen",
+    country = "Pakistan",
+    age = "18";
+
+// challenge 10)
+var myAge = "I am 25 years old.";
+var yourAge = "You are 30 years old.";
+
+
+// .............................................................Congratulations......................................
